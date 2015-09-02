9c56083eead49546e388d44b0f7358f053cbcd89
Brandon Graves <bgraves14@gmail.com>
2015-09-02 14:29:00 +0100

    shoulda

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it { should have_many(:reviews).dependent(:destroy) }
 end
